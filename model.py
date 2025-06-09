c932a4a26ad0129e2d7fbd69c98e9d62642762c8
Haskell <haskell@MacBook-Pro-lain.local>
2025-06-09 12:53:49 +0300

    added method to init db

--- model.py
+++ model.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
 
 db = SQLAlchemy()
 
@@ -7,3 +8,6 @@ class Post(db.Model):
     title = db.Column(db.String(200), nullable=False)
     content = db.Column(db.Text, nullable=False)
     created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+
+def init_app(app: Flask):
+    db.init_app(app)
