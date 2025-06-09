4f3f41db4411f9d1b21a60c889283c27747efc92
Haskell <haskell@MacBook-Pro-lain.local>
2025-06-09 11:13:11 +0300

    made model that represents post

---
+++ model.py
@@ -0,0 +1,9 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
